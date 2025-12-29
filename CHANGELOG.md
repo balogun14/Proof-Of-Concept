b9bded60c36ef11cdf99e580af87f87563f3d1e3
jean-francoisreboud <jean-francois.reboud@owkin.com>
2025-12-29 23:10:06 +0100

    structure

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+All notable changes to this project will be documented in this file.
+
+## [unreleased]
---
+++ tests/test_poc.py
@@ -0,0 +1,8 @@
+import poc
+
+
+def test_poc():
+    """
+    Test that poc library is installed correctly.
+    """
+    print(poc.__version__)
