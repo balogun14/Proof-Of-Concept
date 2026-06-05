51eeea9a5180fcd62801a1151d35e4bd92a3d2f8
Muhammed-Awwal Balogun <awwalbalogun06@gmail.com>
2026-06-05 23:00:04 +0100

    Refactor code structure for improved readability and maintainability

--- poc/config.py
+++ poc/config.py
@@ -11,12 +11,32 @@ class TrainConfig(BaseModel):
     batch_size: int
     lr: float = 1e-5
     max_grad_norm: float | None = None
+    project_name: str | None = None
+    use_wandb: bool = True
+
+
+def get_device() -> str:
+    """
+    Get device to train on.
+
+    Returns
+    -------
+    str
+        Device to train on.
+    """
+    if torch.cuda.is_available():
+        return "cuda"
+    if torch.backends.mps.is_available():
+        return "mps"
+    return "cpu"
 
 
 SMALL_CONFIG = TrainConfig(
-    device="mps",
+    device=get_device(),
     dtype=torch.float32,
     nb_epochs=1,
     batch_size=12,  # 12 64
     max_grad_norm=1.0,
+    project_name="poc",
+    use_wandb=False,
 )
